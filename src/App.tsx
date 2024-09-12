9238bd107828ba1e97b7aa5c8d18fe9ffcb5a1fb
ATILA WILLIAMS DE SOUSA FONSECA <atila.fonseca@ifpa.edu.br>
2024-09-12 11:00:55 -0300

    projeto aula 29

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import Header from './components/Cabecalho'
+import Hero from './components/Hero'
+import ListaVagas from './containers/ListaVagas'
+
+import EstiloGlobal from './global'
+
+function App() {
+  return (
+    <>
+      <EstiloGlobal />
+      <Header />
+      <Hero />
+      <div className="container">
+        <ListaVagas />
+      </div>
+    </>
+  )
+}
+
+export default App
